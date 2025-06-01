cc68f694c2a1c5704f4784d6ea0947dc167e96fa
unik srj <surajrajput733@gmail.com>
2025-06-01 20:16:00 +0530

    add new node file and add new project file in root directory

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(),cssInjectedByJsPlugin()],
    build: {
     sourcemap: true, 
     ssr: false 
